4bd383fca7f629e5c414d6cf89b3c3d1d06ecc93
Dubinkin <dima@dubinkin.ru>
2024-05-02 22:39:03 +0300

    Create project

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 02.05.2024 22:09:04
+ *  Author: dubinkin.ru
+ */ 
+
+#include "main.h"
+
+int main(void)
+{
+    while(1)
+    {
+        
+    }
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ * Created: 02.05.2024 22:09:04
+ *  Author: dubinkin.ru
+ */ 
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <avr/io.h>
+
+#endif /* MAIN_H_ */
