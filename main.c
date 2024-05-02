e9cd0c71d870041ba0f874d03c170ab8b1d39672
Dubinkin <dima@dubinkin.ru>
2024-05-03 00:09:55 +0300

    Input interrupt processing, output pulse generation

--- main.c
+++ main.c
@@ -9,13 +9,20 @@
 #include "timer.h"
 #include "led.h"
 
+uint8_t input_trigger = 0;
+uint32_t sbs_start_time = 0;
+
 void init(void)
 {
 	init_timer();
 	init_led();
 	
-	// checking LED blinking, interval is set to 500 ms
-	blink_led(500);
+	// configuration I/O ports
+	DDRB |= (1 << OUTPUT) | (0 << INPUT);
+	
+	// INT0 falling edge interrupt configuration
+	MCUCR |= (1 << ISC01) | (0 << ISC00);
+	GIMSK |= (1 << INT0);
 	
 	// enable interrupts
 	sei();
@@ -29,5 +36,26 @@ int main(void)
 	{
 		// LED indication processing
 		handler_led();
+		
+		// SBS output switch-off delay
+		if ((millis() - sbs_start_time > 500) && input_trigger)
+		{
+			input_trigger = 0;
+			disable_sbs();
+			disable_led();
+		}
+		
+		_delay_ms(5);
+	}
+}
+
+ISR(INT0_vect)
+{
+	if (!input_trigger)
+	{
+		input_trigger = 1;
+		sbs_start_time = millis();
+		enable_sbs();
+		blink_led(100);
 	}
 }
--- main.h
+++ main.h
@@ -9,5 +9,13 @@
 #define MAIN_H_
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define INPUT 1
+#define OUTPUT 4
+
+#define enable_sbs()	(PORTB |= (1 << OUTPUT))
+#define disable_sbs()	(PORTB &= ~(1 << OUTPUT))
 
 #endif /* MAIN_H_ */
